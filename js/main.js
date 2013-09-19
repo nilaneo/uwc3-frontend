9740cb81a82269ad34b258c37ab2c720a56a86eb
Nila <shlikhtenkoneonila@gmail.com>
2013-09-19 21:46:01 +0300

    Finish left button. Add some JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
+$(function() {
+	var $supportButton = $('.support-button'),
+		$supportWindow = $('.support-window');
 
+	$supportButton.on('click', function() {
+		$supportButton.addClass('support-button-hidden');
+		$supportWindow.fadeIn();
+	});
+
+	$('.button-close').on('click', function() {
+		$supportButton.removeClass('support-button-hidden');
+		$supportWindow.fadeOut();
+	});
+});
