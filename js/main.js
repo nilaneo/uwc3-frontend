6605809119e115a052bdacd954ab293a7812aac9
Nila <shlikhtenkoneonila@gmail.com>
2013-09-23 01:10:57 +0300

    Complete site without RWD

--- js/main.js
+++ js/main.js
@@ -1,14 +1,33 @@
 $(function() {
 	var $supportButton = $('.support-button'),
-		$supportWindow = $('.support-window');
+		$supportWindow = $('.support-window'),
+		$chat = $('.support-window__content__chat'),
+		$email = $('.support-window__content__sending-email'),
+		$title = $('.title-text');
+
 
 	$supportButton.on('click', function() {
+		$email.hide();
+		$chat.show();
+		$title.text('Технічна підтримка слухає...');
 		$supportButton.addClass('support-button-hidden');
 		$supportWindow.fadeIn();
 	});
 
 	$('.button-close').on('click', function() {
-		$supportButton.removeClass('support-button-hidden');
-		$supportWindow.fadeOut();
-	});
+		if ($email.is(':visible')) {
+			$supportButton.removeClass('support-button-hidden');
+			$supportWindow.fadeOut();			
+		} else {
+			$chat.slideUp(400, function() {
+				$title.text('Чат завершено!');
+				$email.slideDown();
+			});			
+		}
+	});	
+
+	$('.nested-file').on('click', function(e) {
+		e.preventDefault();
+		$(this).remove();
+	})
 });
